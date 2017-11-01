40c46bd9a4ccbe2a786bfa92229ee1e5d20a1466
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-11-01 16:00:44 +0000

    Add tests for Updating a product

--- main_test.go
+++ main_test.go
@@ -7,6 +7,8 @@ import (
   "net/http"
   "net/http/httptest"
   "encoding/json"
+  "bytes"
+  "strconv"
   "."
 )
 
@@ -92,11 +94,83 @@ func TestGetNonExistentProduct(t *testing.T) {
   }
 }
 
-// func TestCreateProduct(t *testing.T) {
-//   clearTable()
-//
-//   payload := []byte(`{ "name": "test product", "price": 11.22 }`)
-//
-//
-//
-// }
+func TestCreateProduct(t *testing.T) {
+  clearTable()
+
+  payload := []byte(`{ "name": "test product", "price": 11.22 }`)
+
+  req, _ := http.NewRequest("POST", "/products", bytes.NewBuffer(payload))
+  response := executeRequest(req)
+
+  checkResponseCode(t, http.StatusCreated, response.Code)
+
+  var m map[string]interface{}
+  json.Unmarshal(response.Body.Bytes(), &m)
+
+  if m["name"] != "test product" {
+    t.Errorf("Expected product name to be 'test product'. Got %v", m["name"])
+  }
+
+  if m["price"] != 11.22 {
+    t.Errorf("Expected product price to be 11.22. Got %v", m["price"])
+  }
+
+  // the id is compared to 1.0 because JSON unmarshaling converts numbers to
+  // floats, when the target is a map[string]interface{}
+  if m["id"] != 1.0 {
+    t.Errorf("Expected product ID to be '1'. Got %v", m["id"])
+  }
+}
+
+func TestGetProduct(t *testing.T) {
+  clearTable()
+  addProducts(1)
+
+  req, _ := http.NewRequest("GET", "/products/1", nil)
+  response := executeRequest(req)
+
+  checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+func addProducts(count int) {
+  if count < 1 {
+    count = 1
+  }
+
+  for i := 0; i < count; i++ {
+    a.DB.Exec("INSERT INTO products(name, price) VALUES($1, $2)", "Product " +strconv.Itoa(i), (i+1.0)*10)
+  }
+}
+
+func TestUpdateProduct(t *testing.T) {
+  clearTable()
+  addProducts(1)
+
+  req, _ := http.NewRequest("GET", "/products/1", nil)
+  response := executeRequest(req)
+
+  var originalProduct map[string]interface{}
+  json.Unmarshal(response.Body.Bytes(), &originalProduct)
+
+  payload := []byte{ "name": "updated product", "price": 22.33 }
+
+  // NewBuffer prepares a Buffer to read existing data
+  req, _ := http.NewRequest("PUT", "/products/1", bytes.NewBuffer(payload))
+  response := executeRequest(req)
+
+  var updatedProduct map[string]interface{}
+  json.Unmarshal(response.Body.Bytes(), &updatedProduct)
+
+  if updatedProduct["name"] != "updated product" {
+    t.Errorf("Expected product name to be 'updated product'. Got %v", updatedProduct["name"])
+  }
+
+  if updatedProduct["price"] != 22.33 {
+    t.Errorf("Expected product price to be '22.33'. Got %v", updatedProduct["price"])
+  }
+
+  if updatedProduct["id"] != originalProduct["id"] {
+    t.Errorf("Expected the ID to remain the same (%v). Got %v", originalProduct["id"], updatedProduct["id"])
+  }
+
+}
