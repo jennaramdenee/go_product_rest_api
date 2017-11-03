261a6a345b67027a8842fe24dc35b0010ee02416
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-11-03 18:08:48 +0000

    Fix errors in code

--- app.go
+++ app.go
@@ -2,9 +2,13 @@
 package main
 
 import (
+  "database/sql"
+  "encoding/json"
   "fmt"
   "log"
-  "database/sql"
+  "net/http"
+  "strconv"
+
   "github.com/gorilla/mux"
   _ "github.com/lib/pq"
 )
@@ -28,6 +32,7 @@ func (a *App) Initialize(user, password, dbname string) {
   }
 
   a.Router = mux.NewRouter()
+  a.initializeRoutes()
 }
 
 // Define routes that will use handlers in model.go
@@ -40,4 +45,128 @@ func (a *App) initializeRoutes(){
 }
 
 // Starts the application
-func (a *App) Run(addr string) { }
+func (a *App) Run(addr string) {
+  log.Fatal(http.ListenAndServe(":8000", a.Router))
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+  respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+  response, _ := json.Marshal(payload)
+
+  w.Header().Set("Content-Type", "application/json")
+  w.WriteHeader(code)
+  w.Write(response)
+}
+
+// Handler method for a single product
+func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
+  // Get id of the product from requested URL
+  vars := mux.Vars(r)
+  // Atoi is string to int
+  id, err := strconv.Atoi(vars["id"])
+  if err != nil {
+    respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+    return
+  }
+
+  p := product{ ID: id }
+  // Uses the getProduct method defined above
+  if err := p.getProduct(a.DB); err != nil {
+    switch err {
+    case sql.ErrNoRows:
+      respondWithError(w, http.StatusNotFound, "Product not found")
+    default:
+      respondWithError(w, http.StatusInternalServerError, err.Error())
+    }
+    return
+  }
+  respondWithJSON(w, http.StatusOK, p)
+}
+
+func (a *App) createProduct(w http.ResponseWriter, r *http.Request){
+  var p product
+  // Decoding a stream of data - assumes that the input is in JSON format
+  decoder := json.NewDecoder(r.Body)
+  if err := decoder.Decode(&p); err != nil {
+    respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+    return
+  }
+  // Remember to close stream
+  defer r.Body.Close()
+
+  if err := p.createProduct(a.DB); err != nil {
+    respondWithError(w, http.StatusInternalServerError, err.Error())
+    return
+  }
+
+  respondWithJSON(w, http.StatusCreated, p)
+}
+
+func (a *App) updateProduct(w http.ResponseWriter, r *http.Request){
+  vars := mux.Vars(r)
+  id, err := strconv.Atoi(vars["id"])
+  if err != nil {
+    respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+    return
+  }
+
+  var p product
+  decoder := json.NewDecoder(r.Body)
+  if err := decoder.Decode(&p); err != nil {
+    respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+    return
+  }
+  defer r.Body.Close()
+  p.ID = id
+
+  if err := p.updateProduct(a.DB); err != nil {
+    respondWithError(w, http.StatusInternalServerError, err.Error())
+    return
+  }
+
+  respondWithJSON(w, http.StatusOK, p)
+}
+
+func (a *App) deleteProduct(w http.ResponseWriter, r *http.Request){
+  vars := mux.Vars(r)
+  id, err := strconv.Atoi(vars["id"])
+  if err != nil {
+    respondWithError(w, http.StatusBadRequest, "ID not valid")
+    return
+  }
+
+  p := product{ID:id}
+  if err := p.deleteProduct(a.DB); err != nil {
+    respondWithError(w, http.StatusInternalServerError, err.Error())
+    return
+  }
+
+  respondWithJSON(w, http.StatusOK, map[string]string{ "result": "success"})
+}
+
+// Handler method for many products
+func (a *App) getProducts(w http.ResponseWriter, r *http.Request) {
+  // get count and start parameters from querystring
+  // r.FormValue returns the first value for the named component of the query
+  count, _ := strconv.Atoi(r.FormValue("count"))
+  start, _ := strconv.Atoi(r.FormValue("start"))
+
+  if count > 10 || count < 1 {
+    count = 10
+  }
+
+  if start < 0 {
+    start = 0
+  }
+
+  products, err := getProducts(a.DB, start, count)
+  if err != nil {
+    respondWithError(w, http.StatusInternalServerError, err.Error())
+    return
+  }
+
+  respondWithJSON(w, http.StatusOK, products)
+}
--- model.go
+++ model.go
@@ -3,7 +3,6 @@ package main
 
 import (
   "database/sql"
-  "strconv"
 )
 
 type product struct {
@@ -37,7 +36,7 @@ func (p *product) createProduct(db *sql.DB) error {
 }
 
 // Functions that deal with many products
-func (p *product) getProducts(db *sql.DB, start, count int) ([]product, error) {
+func getProducts(db *sql.DB, start, count int) ([]product, error) {
   // Fetch records from the products table, limiting number of records by count, and skipping the number of records defined by start
   rows, err := db.Query("SELECT id, name, price FROM products LIMIT $1 OFFSET $2", count, start)
   if err != nil {
@@ -58,124 +57,4 @@ func (p *product) getProducts(db *sql.DB, start, count int) ([]product, error) {
     products = append(products, p)
   }
   return products, nil
-}
-
-// Handler method for a single product
-func (a *App) getProduct(w http.ResponseWriter, r *http.Request) {
-  // Get id of the product from requested URL
-  vars := mux.Vars(r)
-  // Atoi is string to int
-  id, err := strconv.Atoi(vars["id"])
-  if err != nil {
-    respondWithError(w, http.StatusBadRequest, "Invalid product ID")
-    return
-  }
-
-  p := product{ ID: id }
-  // Uses the getProduct method defined above
-  if err := p.getProduct(a.DB); err != nil {
-    switch err {
-    case sql.ErrNoRows:
-      respondWithError(w, http.StatusNotFound, "Product not found")
-    default:
-      respondWithError(w, http.StatusInternalServerError, err.Error())
-    }
-    return
-  }
-  respondWithJSON(w, http.StatusOK, p)
-}
-
-func respondWithError(w http.ResponseWriter, code int, message string) {
-  respondWithJSON(w, code, map[string]string{"error": message})
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-  response, _ := json.Marshal(payload)
-
-  w.Header().Set("Content-Type", "application/json")
-  w.WriteHeader(code)
-  w.Write(response)
-}
-
-func (a *App) createProduct(w http.ResponseWriter, r *http.Request){
-  var p product
-  // Decoding a stream of data - assumes that the input is in JSON format
-  decoder := json.NewDecoder(r.Body)
-  if err := decoder.Decode(&p); err != nil {
-    respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-    return
-  }
-  // Remember to close stream
-  defer r.Body.Close()
-
-  if err := p.createProduct(a.DB); err != nil {
-    respondWithError(w, http.StatusInternalServerError, err.Error())
-  }
-
-  respondWithJSON(w, http.StatusCreated, p)
-}
-
-func (a *App) updateProduct(w http.ResponseWriter, r *http.Request){
-  vars := mux.Vars(r)
-  id, err := strconv.Atoi(vars["id"])
-  if err != nil {
-    respondWithError(w, http.StatusBadRequest, "Invalid product ID")
-    return
-  }
-
-  var p product
-  decoder := json.NewDecoder(r.Body)
-  if err := decoder.Decode(&p); err != nil {
-    respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-    return
-  }
-  defer r.Body.Close()
-  p.ID = id
-
-  if err := p.updateProduct(a.DB); err != nil {
-    respondWithError(w, http.StatusInternalServerError, err.Error())
-    return
-  }
-
-  respondWithJSON(w, http.StatusOK, p)
-}
-
-func (a *App) deleteProduct(w http.ResponseWriter, r *http.Request){
-  vars := mux.Vars(r)
-  id, err := strconv.Atoi(vars["id"])
-  if err != nil {
-    respondWithError(w, http.StatusBadRequest, "ID not valid")
-    return
-  }
-
-  p := product{ID: id}
-  err := p.deleteProduct(a.DB); err != nil {
-    respondWithError(w, http.StatusInternalServerError, err.Error())
-    return
-  }
-
-  respondWithJSON(w, http.StatusOK, map[string]string{ "result": "success"})
-}
-// Handler method for many products
-func (a *App) getProducts(w http.ResponseWriter, r *http.Request) {
-  // get count and start parameters from querystring
-  // r.FormValue returns the first value for the named component of the query
-  count = strconv.Atoi(r.FormValue("count"))
-  start = strconv.Atoi(r.FormValue("start"))
-
-  if count > 10 || count < 1 {
-    count = 10
-  }
-
-  if start < 0 {
-    start = 0
-  }
-
-  products, err := p.getProduct(a.DB, start, count)
-  if err != nil {
-    respondWithError(w, http.StatusInternalServerError, err.Error())
-    return
-  }
-
-  respondWithJSON(w, http.StatusOK, products)
 }
